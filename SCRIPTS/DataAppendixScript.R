27d413eecba1f6c1b1a927df5e250010413f3f26
natalieassaad <108235343+natalieassaad@users.noreply.github.com>
2025-03-21 14:08:34 -0400

    Add files via upload

---
+++ SCRIPTS/DataAppendixScript.R
@@ -0,0 +1,33 @@
+#===========================================================
+# This document contains our data appendix script. It provides
+# summary measures and visualizations for the main variables 
+# used in our analysis. 
+#===========================================================
+
+library(ggplot2)
+
+Data <- read.csv("/Users/Nata/Desktop/Cleaned Aviation Data Final.csv")
+
+# Visualize Year Distribution
+table(Data$Year)
+
+ggplot(Data, aes(x=Year, fill=as.factor(Year))) + 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="none")
+
+# Visualize Month distribution (1-12, 1 for January, 2 for February, etc.)
+table(Data$Month)
+
+ggplot(Data, aes(x=Month, fill=Month)) + 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="none")
+
+
+# Visualize FatalInjuryCount
+summary(Data$FatalInjuryCount)
+
+hist(aviation_data$FatalInjuryCount, breaks=25, xlim=c(0,15), col=rgb(1,0,0,0.5), 
+     xlab = "Fatal Injury Count",
+     main="Distribution of Fatal Injury Count" )
